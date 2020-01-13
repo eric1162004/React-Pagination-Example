e2d9046e52eafb2e74ccf841350127931ba2e574
Eric <eric1162004@hotmail.com>
2020-01-13 13:58:23 -0800

    Updates

---
+++ precache-manifest.46df69ea048bf2a60d2ac00f281a7795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3487ab4ba1044ef6c399054b48f62d49",
+    "url": "/eric1162004/React-Pagination-Example.git/index.html"
+  },
+  {
+    "revision": "c6a5cc72bb752883eb7d",
+    "url": "/eric1162004/React-Pagination-Example.git/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "7a8158ba5d05b3e4c2a5",
+    "url": "/eric1162004/React-Pagination-Example.git/static/js/2.dbb36bbe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/eric1162004/React-Pagination-Example.git/static/js/2.dbb36bbe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6a5cc72bb752883eb7d",
+    "url": "/eric1162004/React-Pagination-Example.git/static/js/main.2d85e8b1.chunk.js"
+  },
+  {
+    "revision": "0b04850b4c55111b6a6e",
+    "url": "/eric1162004/React-Pagination-Example.git/static/js/runtime-main.5cd6363e.js"
+  }
+]);
